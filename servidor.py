3fd85259d6848c9a9fb42943daf2c4c4d6c41e10
joaquim.fg <joaquimfaguiar@outlook.com>
2026-03-16 22:14:01 -0300

    tests corrigidos

--- servidor.py
+++ servidor.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from utils import load_templates, load_db, init_db
+from utils import load_data, load_templates, load_db, init_db
 app = Flask(__name__)
 
 init_db()
 
 @app.route("/")
-def listar_imoveis():
-    pass
+def listar():
+    imoveis = load_data()
+
+
+
+
     
 
 if __name__ == "__main__":
