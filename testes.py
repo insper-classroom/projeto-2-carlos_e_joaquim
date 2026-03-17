6a6847e179078024d9e75479e56ff58afaa6502d
joaquim.fg <joaquimfaguiar@outlook.com>
2026-03-16 23:00:26 -0300

    test listar imoveis por id

--- testes.py
+++ testes.py
@@ -60,17 +60,53 @@ def test_listar_imoveis_vazio(mock_conectar_banco, client):
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
 
-def test_criar_imovel(client):
-    """POST /criar-imovel - criar imóvel."""
-    response = client.post("/criar-imovel", json={
-        "logradouro": "John Falls",
-        "tipo_logradouro": "Rua",
-        "bairro": "Port Carol",
-        "cidade": "Knappview",
-        "cep": "14150",
-        "tipo": "casa",
-        "valor": 961722.89,
-        "data_aquisicao": "2022-01-05"
-    })
-    assert response.status_code == 201
-    assert response.get_json() == {"message": "Imóvel criado com sucesso!"}
+@patch("api.conectar_banco")
+def test_listar_imoveis_pelo_id(mock_conectar_banco, client):
+    """GET /listar-imoveis - listar imóvel pelo id."""
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.fetchall.return_value = [
+        ('John Falls', 'Rua', 'Port Carol', 'Knappview', '14150', 'casa', 961722.89, '2022-01-05'),
+    ]
+
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.get("/listar-imoveis?id=1")
+
+    assert response.status_code == 200
+    assert response.get_json() == [
+        {"logradouro": "John Falls", "tipo_logradouro": "Rua", "bairro": "Port Carol", "cidade": "Knappview", "cep": "14150", "tipo": "casa", "valor": 961722.89, "data_aquisicao": "2022-01-05"},
+    ]
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE id = %s",
+        (1,)
+    )
+    mock_cursor.fetchall.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
+
+def test_listar_imoveis_pelo_id_inexistente(mock_conectar_banco, client):
+    """GET /listar-imoveis - listar imóvel pelo id inexistente."""
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.fetchall.return_value = []
+
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.get("/listar-imoveis?id=999")
+
+    assert response.status_code == 200
+    assert response.get_json() == []
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE id = %s",
+        (999,)
+    )
+    mock_cursor.fetchall.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
